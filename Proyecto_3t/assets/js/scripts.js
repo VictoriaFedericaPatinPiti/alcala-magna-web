c44214298bd6711d9e6c03a9a7acec14b4f767fa
Álvaro Sánchez de la Vauqera Mercant <asanchezvaqueram@alumnos.nebrija.es>
2025-06-05 08:52:59 +0200

    Delete Proyecto_3t directory

--- Proyecto_3t/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('load-more').addEventListener('click', function() {
-    const eventosList = document.getElementById('eventos-list');
-    const newEvent = document.createElement('li');
-    newEvent.textContent = 'Nuevo evento agregado - 25 de septiembre';
-    eventosList.appendChild(newEvent);
-});
